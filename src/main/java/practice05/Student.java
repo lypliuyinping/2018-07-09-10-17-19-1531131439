a91ad11649606839f8bcecdd423f8c530d369187
liuyinping <1689372042@qq.com>
2019-08-23 00:47:14 +0800

    5

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,25 @@
 package practice05;
 
-public class Student {
+
+
+public class Student extends Person{
+	private	String klass;
+	public Student(String name, String age,String klass) {
+		super(name, age);
+		this.klass=klass;
+		// TODO Auto-generated constructor stub
+	}
+	public void introduce(String name,String age,String klass) {
+		System.out.println("My name is " + name +". I am "+age+
+				" years old. I am a Student. I am at Class"+  klass);
+	}
+	public static void main(String[] args) {
+		Person person = new Person("Tom","21");
+		person.introduce("Tom","21");
+		Student student = new Student("Tom","21","1");
+		student.introduce("Tom","21","2");	
+	}
+
+
+
 }
