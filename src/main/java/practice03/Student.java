a0f9ef267983d034e727c5f195bc79214aab1fd8
liuyinping <1689372042@qq.com>
2019-08-23 00:24:00 +0800

    3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,20 @@
 package practice03;
 
-public class Student {
+public class Student  extends Person{
+	private	String klass;
+	public Student(String name, String age,String klass) {
+		super(name, age);
+		this.klass=klass;
+		// TODO Auto-generated constructor stub
+	}
+	public void introduce(String name,String age,String klass) {
+		System.out.println("I am a Student." + "I am at " + klass);
+	}
+	public static void main(String[] args) {
+		Person person = new Person("Tom","21");
+		person.introduce("Tom","21");
+		Student student = new Student("Tom","21","1");
+		student.introduce("Tom","21","1");	
+	}
+	
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,20 @@
 package practice03;
 
-public class Worker {
+import practice02.Person;
+import practice02.Student;
+
+public class Worker extends Person {
+
+	public Worker(String name, String age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	public void introduce(String name,String age) {
+		System.out.println("I am a Worker." + "I have a job " );
+	}
+	public static void main(String[] args) {
+		Worker work = new Worker("Tom","21");
+		work.introduce("Tom","21");
+		
+	}
 }
