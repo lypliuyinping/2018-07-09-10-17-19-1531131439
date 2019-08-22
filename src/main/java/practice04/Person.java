2d595f907c421a58bc3c79f61b850c779c44619f
liuyinping <1689372042@qq.com>
2019-08-23 00:40:56 +0800

    4

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,17 @@
 package practice04;
 
 public class Person {
+	String name;
+	String age;
+	public Person(String name,String age) {
+		this.name = name;
+		this.age = age;
+	}
+	public void basicIntroduce(String name,String age) {
+		System.out.println("My name is"+ name+". I am " +age+" years old.");
+	}
+	public static void main(String[] args) {
+		Person person = new Person("Tom","21");
+		person.basicIntroduce("Tom","21");	
+	}
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Worker {
+public class Worker  extends Person {
+
+	public Worker(String name, String age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	public void introduce(String name,String age) {
+		System.out.println("My name is " + name +". I am "+age+" years old."+
+				" I am a Worker. I have a job.");
+	}
+	public static void main(String[] args) {
+		Worker work = new Worker("Tom","21");
+		work.basicIntroduce("Tom","21");
+		
+	}
 }
