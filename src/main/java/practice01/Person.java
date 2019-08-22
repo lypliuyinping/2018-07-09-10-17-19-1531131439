0a218e76f5a8edb081e64599cbfd9b5b78d9c7df
liuyinping <1689372042@qq.com>
2019-08-22 21:18:40 +0800

    1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,18 @@
 package practice01;
 
 public class Person {
+	String name;
+	String age;
+	public Person(String name,String age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void introduce(String name,String age) {
+		System.out.println("My name is"+ name+". I am " +age+" years old.");
+	}
+	 public static void main(String[] args) {
+	    	Person person = new Person("Tom","21");
+	    	person.introduce("Tom","21");	
+	    }
 }
