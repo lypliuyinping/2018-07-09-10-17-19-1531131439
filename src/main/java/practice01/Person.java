75eea87adaea5a0604c972f839e9ff6129095578
liuyinping <1689372042@qq.com>
2019-08-23 10:10:17 +0800

    modify 1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,18 +1,34 @@
 package practice01;
 
 public class Person {
-	String name;
-	String age;
-	public Person(String name,String age) {
+	private String name;
+	private int age;
+	public Person(String name,int age) {
 		this.name = name;
 		this.age = age;
 	}
-	
-	public void introduce(String name,String age) {
-		System.out.println("My name is"+ name+". I am " +age+" years old.");
+		
+	public String getName() {
+		return name;
 	}
-	 public static void main(String[] args) {
-	    	Person person = new Person("Tom","21");
-	    	person.introduce("Tom","21");	
-	    }
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String introduce() {
+		return "My name is "+ this.name+". I am " +this.age+" years old.";
+	}
+//	 public static void main(String[] args) {
+//	    	Person person = new Person("Tom","21");
+//	    	person.introduce("Tom","21");	
+//	    }
 }
